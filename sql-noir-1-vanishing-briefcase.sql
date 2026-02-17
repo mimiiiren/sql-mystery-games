c384571f4e37b21802c1946d6e06fc1fd8fe1fbe
mimiiiren <mimiren015@gmail.com>
2026-02-17 17:10:16 -0500

    add noir 1 and 2

--- sql-noir-1-vanishing-briefcase.sql
+++ sql-noir-1-vanishing-briefcase.sql
@@ -2,3 +2,23 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+SELECT * 
+FROM crime_scene
+WHERE type = "theft" 
+AND date >= 19800000
+AND date < 19900000;
+A briefcase containing sensitive documents vanished. A witness reported a man in a trench coat with a scar on his left cheek fleeing the scene.
+
+SELECT * 
+FROM suspects
+WHERE attire LIKE '%trench coat%'
+AND scar LIKE '%left cheek%';
+id	name	attire	scar
+3	Frankie Lombardi	trench coat	left cheek
+183	Vincent Malone	trench coat	left cheek
+
+SELECT * 
+FROM interviews
+WHERE suspect_id = 183;
+ANSWER: Vincent Malone
--- sql-noir-2-stolen-sound.sql
+++ sql-noir-2-stolen-sound.sql
@@ -2,3 +2,36 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+SELECT *
+FROM crime_scene
+WHERE date = 19830715;
+A prized vinyl record was stolen from the store during a busy evening.
+
+SELECT *
+FROM witnesses
+WHERE id = 65;
+
+SELECT *
+FROM witnesses
+WHERE crime_scene_id = 65;
+id	crime_scene_id	clue
+28	65	I saw a man wearing a red bandana rushing out of the store.
+75	65	The main thing I remember is that he had a distinctive gold watch on his wrist.
+
+SELECT *
+FROM suspects
+WHERE bandana_color = "red"
+AND accessory = 'gold watch';
+id	name	bandana_color	accessory
+35	Tony Ramirez	red	gold watch
+44	Mickey Rivera	red	gold watch
+97	Rico Delgado	red	gold watch
+
+SELECT *
+FROM interviews
+WHERE suspect_id = 97;
+
+SELECT *
+FROM suspects
+WHERE id = 97;
+Answer: Rico Delgado
