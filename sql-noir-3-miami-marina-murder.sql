c35b3f222f86969272ce60d2f070733fd34d474b
mimiiiren <mimiren015@gmail.com>
2026-02-19 17:15:45 -0500

    solved sql-noir 3 and 4'

--- sql-noir-3-miami-marina-murder.sql
+++ sql-noir-3-miami-marina-murder.sql
@@ -2,3 +2,74 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+SELECT *
+FROM crime_scene
+WHERE location = 'Coral Bay Marina' AND date = 19860814;
+
+id	date	location	description
+43	19860814	Coral Bay Marina	The body of an 
+unidentified man was found near the docks. 
+Two people were seen nearby: one who lives on 
+300ish "Ocean Drive" and another whose first name 
+ends with "ul" and his last name ends with "ez".
+
+SELECT *
+FROM person
+WHERE address LIKE '% Ocean Drive' OR name LIKE '%ul %ez%';
+101	Carlos Mendez	Los Ojos	Fisherman	369 Ocean Drive
+102	Raul Gutierrez	The Cobra	Nightclub Owner	45 Sunset Ave
+
+-- SELECT *
+-- FROM confessions
+-- WHERE person_id = 101 OR person_id = 102;
+
+-- id	person_id	confession
+-- 96	101	This is just a misunderstanding.
+-- 97	102	Alright! Ive been running a blackmail operation
+
+-- -- SELECT *
+-- -- FROM surveillance_records
+-- -- WHERE person_id = 101 OR person_id = 102;
+-- -- id	person_id	hotel_checkin_id	suspicious_activity
+-- -- 101	101	45	Asked for room service menu
+-- -- 102	102	12	NULL
+
+-- -- SELECT *
+-- -- FROM hotel_checkins
+-- -- WHERE id = 45 OR id = 12;
+
+-- -- id	person_id	hotel_name	check_in_date
+-- -- 12	17	Sunset Shore Hotel	19860813
+-- -- 45	79	Coastal View Hotel	19860813
+
+SELECT *
+FROM interviews
+WHERE person_id = 101 OR person_id = 102;
+id	person_id	transcript
+101	101	I saw someone check into a hotel on August 13. The guy looked nervous.
+103	102	I heard someone checked into a hotel with "Sunset" in the name.
+
+ SELECT *
+   FROM hotel_checkins
+WHERE check_in_date = 19860813 AND hotel_name LIKE '%Sunset%'
+
+   SELECT *
+   FROM hotel_checkins
+JOIN surveillance_records
+ON hotel_checkins.id = surveillance_records.hotel_checkin_id
+WHERE check_in_date = 19860813 AND hotel_name LIKE '%Sunset%'
+---return MANY results. how to narrow down?
+34	39	Sunset Coast Inn	19860813	6	6	34	Spotted entering late at night
+2	27	Sunset Bay Hotel	19860813	8	8	2	Left suddenly at 3 AM
+
+
+SELECT *
+FROM hotel_checkins
+WHERE person_id = 8;
+id	person_id	hotel_name	check_in_date
+55	8	Sunset Palm Resort	19860813
+
+id	name	alias	occupation	address
+8	Thomas Brown	The Fox	Dock Worker	234 Port Street
+
+Answer: Thomas Brown
--- sql-noir-4-vanishing-diamond.sql
+++ sql-noir-4-vanishing-diamond.sql
@@ -2,3 +2,94 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+SELECT *
+FROM crime_scene
+WHERE location = 'Fontainebleau Hotel';
+id	date	location	description
+48	19870520	Fontainebleau Hotel	The Heart of Atlantis necklace disappeared. 
+Many guests were questioned but only two of them gave valuable clues. 
+One of them is a really famous actor. 
+The other one is a woman who works as a consultant for a big company and her first name 
+ends with "an".
+
+SELECT *
+FROM guest
+WHERE occupation = 'Actor' 
+id	name	occupation	invitation_code
+43	Ruby Baker	Actor	VIP-R
+129	Clint Eastwood	Actor	VIP-G
+164	River Bowers	Actor	VIP-B
+189	Sage Dillon	Actor	VIP-G
+192	Phoenix Pitts	Actor	VIP-G
+SELECT *
+FROM witness_statements
+WHERE guest_id IN (143, 129, 164, 189, 192);
+
+id	guest_id	clue
+50	129	I overheard someone say, Meet me at the marina dock 3
+
+SELECT *
+FROM guest
+WHERE occupation LIKE '%Consultant%' AND name LIKE '%an%';
+
+id	name	occupation	invitation_code
+116	Vivian Nair	Consultant	VIP-R
+
+SELECT *
+FROM witness_statements
+WHERE guest_id = 116;
+id	guest_id	clue
+16	116	I saw someone holding an invitation ending with "-R". 
+He was wearing a navy suit and a white tie.
+
+SELECT *
+FROM attire_registry
+WHERE note LIKE '%white tie%';
+
+id	guest_id	note
+30	156	navy suit, white tie
+42	145	navy suit, white tie
+48	105	navy suit, white tie
+53	112	navy suit, white tie
+58	190	navy suit, white tie
+61	189	navy suit, white tie
+63	167	navy suit, white tie
+67	190	navy suit, white tie
+
+-- SELECT *
+-- FROM marina_rentals
+-- JOIN attire_registry
+-- ON marina_rentals.renter_guest_id = attire_registry.guest_id
+-- WHERE marina_rentals.dock_number = 3 AND attire_registry.note LIKE '%white tie%';
+-- id	dock_number	renter_guest_id	rental_date	boat_name	id	guest_id	note
+-- 9	3	112	19870520	Sea Serenade	53	112	navy suit, white tie
+-- 19	3	145	19870520	Wave Seeker	42	145	navy suit, white tie
+-- 24	3	167	19870520	Wind Song	63	167	navy suit, white tie
+-- 34	3	190	19870520	Ocean Spirit	58	190	navy suit, white tie
+-- 34	3	190	19870520	Ocean Spirit	67	190	navy suit, white tie
+-- 44	3	105	19870520	Coastal Spirit	48	105	navy suit, white tie
+-- 54	3	112	19871028	Blue Spirit	53	112	navy suit, white tie
+-- 64	3	167	19871028	Wind Moon	63	167	navy suit, white tie
+-- 74	3	156	19871028	Ocean Star II	30	156	navy suit, white tie
+-- 79	3	112	19871028	Sunset Wave II	53	112	navy suit, white tie
+-- 84	3	145	19871028	Coastal Lady	42	145	navy suit, white tie
+
+-- SELECT *
+-- FROM witness_statements
+-- WHERE guest_id IN (112, 145, 167, 190, 105, 112, 167, 156, 112, 145);
+-- id	guest_id	clue
+-- 3	167	During my evening jog, I noticed a black van parked outside with its lights off for over two hours.
+
+SELECT *
+FROM final_interviews
+WHERE guest_id IN (112, 145, 167, 190, 105, 112, 167, 156, 112, 145);
+id	guest_id	confession
+105	105	I was the one who took the crystal. I guess I need a lawyer now?
+
+SELECT *
+FROM guest
+WHERE id = 105;
+105	Mike Manning	Wealth Reallocation Expert	VIP-R
+
+ANSWER: Mike Manning	
